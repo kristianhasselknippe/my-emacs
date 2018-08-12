287ec91745b17ae726903c76084cfea89eeed59e
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2018-08-12 14:00:51 +0200

    Made config work with emacs-26.1

--- custom-vars.el
+++ custom-vars.el
@@ -3,101 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(company-auto-complete-chars (quote (32 95 40 41 119 46 34 36 39 47)))
- '(company-frontends
-   (quote
-	(company-pseudo-tooltip-frontend company-echo-metadata-frontend)))
- '(company-minimum-prefix-length 2)
- '(company-require-match nil)
- '(compilation-scroll-output t)
- '(cursor-type (quote bar))
- '(custom-enabled-themes (quote (monokai)))
- '(custom-safe-themes
-   (quote
-	("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "d3a406c5905923546d8a3ad0164a266deaf451856eca5f21b36594ffcb08413a" "557c283f4f9d461f897b8cac5329f1f39fac785aa684b78949ff329c33f947ec" "c567c85efdb584afa78a1e45a6ca475f5b55f642dfcd6277050043a568d1ac6f" "1e3b2c9e7e84bb886739604eae91a9afbdfb2e269936ec5dd4a9d3b7a943af7f" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default)))
- '(delete-selection-mode 1)
- '(display-line-numbers t)
- '(edebug-on-error t)
- '(flycheck-javascript-flow-args nil)
- '(global-auto-complete-mode t)
- '(global-whitespace-mode t)
- '(helm-completing-read-dynamic-complete nil)
- '(helm-completing-read-handlers-alist
-   (quote
-	((describe-function . helm-completing-read-symbols)
-	 (describe-variable . helm-completing-read-symbols)
-	 (describe-symbol . helm-completing-read-symbols)
-	 (debug-on-entry . helm-completing-read-symbols)
-	 (find-function . helm-completing-read-symbols)
-	 (disassemble . helm-completing-read-symbols)
-	 (trace-function . helm-completing-read-symbols)
-	 (trace-function-foreground . helm-completing-read-symbols)
-	 (trace-function-background . helm-completing-read-symbols)
-	 (find-tag . helm-completing-read-default-find-tag)
-	 (org-capture . helm-org-completing-read-tags)
-	 (org-set-tags . helm-org-completing-read-tags)
-	 (ffap-alternate-file)
-	 (tmm-menubar)
-	 (find-file)
-	 (find-file-at-point . helm-completing-read-sync-default-handler)
-	 (ffap . helm-completing-read-sync-default-handler)
-	 (execute-extended-command)
-	 (dired-do-rename . helm-read-file-name-handler-1)
-	 (dired-do-copy . helm-read-file-name-handler-1)
-	 (dired-do-symlink . helm-read-file-name-handler-1)
-	 (dired-do-relsymlink . helm-read-file-name-handler-1)
-	 (dired-do-hardlink . helm-read-file-name-handler-1)
-	 (dired-do-rename)
-	 (dired-do-copy))))
- '(js2-strict-missing-semi-warning nil)
- '(magit-diff-options nil)
- '(magit-pull-arguments nil)
- '(magit-use-overlays nil)
- '(markdown-command "/usr/local/bin/pandoc")
- '(menu-bar-mode nil)
- '(monokai-high-contrast-mode-line t)
- '(neo-window-width 40)
- '(nxml-child-indent 4)
- '(omnisharp-auto-complete-want-documentation nil)
- '(omnisharp-server-executable-path "~/omnisharp-server/OmniSharp.exe")
- '(org-agenda-files (quote ("~/Jottacloud/org")))
- '(org-startup-indented t)
- '(org-startup-truncated t)
  '(package-selected-packages
    (quote
-	(yasnippet-snippets hydra company-restclient restclient restclient-mode magithub dotnet rg urlenc use-package ## helm helm-core ivy projectile company lsp-ui edit-color-stamp web-mode omnisharp omnisharp-mode ts-comint expand-region wgrep ht reveal-in-osx-finder glsl-mode cargo dart-mode magit avy-zap uniquify ace-window tide counsel typescript-mode haxe-mode monokai swiper editorconfig edebug-x company-lsp imenu-anywhere lsp-mode realgud yasnippet yaml-mode windata websocket undo-tree tree-mode toml-mode tabbar-ruler subatomic-theme spotify smex smart-mode-line slime shut-up ripgrep rainbow-mode popup-complete pdf-tools paredit-menu paredit-everywhere ov nodejs-repl neotree multiple-cursors multi monokai-theme mmm-mode minimap markdown-mode magit-gitflow magit-gh-pulls lua-mode lorem-ipsum json-mode js2-mode jdee irony helm-projectile graphviz-dot-mode fsharp-mode flycheck-rust flycheck-flow exec-path-from-shell emms el-get dash-functional cuda-mode csharp-mode avy adaptive-wrap ace-jump-mode ac-racer ac-ispell ac-cider)))
- '(projectile-globally-ignored-directories
-   (quote
-	(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "node_modules" "Pods")))
- '(projectile-project-root-files
-   (quote
-	("rebar.config" "project.clj" "build.boot" "SConstruct" "pom.xml" "build.sbt" "gradlew" "build.gradle" ".ensime" "Gemfile" "requirements.txt" "setup.py" "tox.ini" "composer.json" "Cargo.toml" "mix.exs" "stack.yaml" "info.rkt" "DESCRIPTION" "TAGS" "GTAGS" "*.unoproj")))
- '(ring-bell-function (quote ignore))
- '(ripgrep-arguments (quote ("")))
- '(rustup-executable rustup)
- '(scroll-bar-mode nil)
- '(show-paren-mode t)
- '(tide-allow-popup-select (quote (code-fix jump-to-implementation refactor)))
- '(tide-completion-detailed nil)
- '(tide-completion-ignore-case t)
- '(tool-bar-mode nil)
- '(ts-comint-program-command "ts-node")
- '(typescript-auto-indent-flag nil)
- '(whitespace-line-column 20000)
- '(whitespace-style
-   (quote
-	(face trailing tabs spaces lines empty indentation space-after-tab space-before-tab space-mark tab-mark))))
+    (calfw-org calfw yasnippet-snippets cargo avy-zap ace-window editorconfig tide multiple-cursors helm-projectile rainbow-mode counsel helm web-mode typescript-mode rg swiper omnisharp yaml-mode restclient eglot company-lsp nodejs-repl json-mode js2-mode undo-tree smex fsharp-mode company magit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-face-highlight-textual ((t (:background "blue"))))
- '(whitespace-indentation ((t (:foreground "gray29" :weight bold))))
- '(whitespace-tab ((t (:foreground "gray31" :weight bold))))
- '(whitespace-trailing ((t (:foreground "#59413d" :inverse-video t)))))
-
-
-(setq company-dabbrev-downcase 0)
-(setq company-idle-delay 0)
+ )
--- init.el
+++ init.el
@@ -1,38 +1,30 @@
 (require 'package)
-
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-					(not (gnutls-available-p))))
-	   (proto (if no-ssl "http" "https")))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
-	;; For important compatibility libraries like cl-lib
-	(add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
-
-
-(setq package-list '(use-package auto-complete ac-cider flycheck hideshow sgml-mode auto-complete-config company
-				   smex paredit-menu undo-tree highlight-focus dired-x graphviz-dot-mode
-				   nodejs-repl rust-mode browse-url flycheck-flow yaml-mode dotnet company-restclient
-				   org-gcal calfw-org calfw))
-
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
-
-
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+(require 'use-package)
 
-; install the missing packages
-;(dolist (package package-list)
-;  (unless (package-installed-p package)
-										;    (package-install package)))
+(defun my-magit-mode-setup ()
+  (define-key magit-mode-map (kbd "M-p") 'ace-window))
 
-(setq exec-path (append "/usr/local/Cellar/aspell/0.60.6.1/bin/" exec-path))
-;(load-file "~/.emacs.d/reveal-in-finder.el")
-;(load-file "~/.emacs.d/dired-details.el")
+(use-package magit
+  :ensure t
+  :bind ("C-c m" . 'magit-status)
+  :config
+  (setq magit-auto-revert-mode nil)
+  (add-hook 'magit-mode-hook 'my-magit-mode-setup))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq delete-by-moving-to-trash t)
@@ -46,41 +38,16 @@
 
 (set-face-foreground 'font-lock-comment-face "light pink")
 (setq-default c-basic-offset 4)
-
 (setq default-tab-width 4)
-
-(eval-after-load "dired-aux"
-  '(add-to-list 'dired-compress-file-suffixes
-				'("\\.zip\\'" ".zip" "unzip")))
-
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin" ":/Users/Hassel/.cargo/bin"))
-(setq exec-path (append exec-path '("/usr/local/bin")))
-(setq exec-path (append exec-path '("/Users/Hassel/.cargo/bin")))
-(setq exec-path (append exec-path '("/Library/Frameworks/Mono.framework/Versions/Current/bin/")))
-
-(setq exec-path (append "/usr/local/Cellar/aspell/0.60.6.1/bin/" exec-path))
-(setq ispell-program-name "aspell")
-(setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))
-
-(setq-default indent-tabs-mode t)
-
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-
-;(defun insert-random-material-color ()
-;  (interactive)
-;  (let*
-;	  ((colors-length (length material-colors))
-;	   (i (random colors-length)))
-;	(insert (nth i material-colors))))
-
-(require 'use-package)
-
-(require 'cl)
 
 (use-package company
   :ensure t
   :hook (typescript-mode))
+(defun setup-company-mode ()
+  (setq company-tooltip-align-annotations t)
+  (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort))
+(add-hook 'company-mode-hook #'setup-company-mode)
 
 (use-package nxml-mode
   :mode "\\.ux\\'"
@@ -90,7 +57,7 @@
 (use-package asm-mode
   :mode "\\.as\\'"
   :bind (:map asm-mode-map
-			  ("<f5>" . #'compile)))
+	      ("<f5>" . #'compile)))
 
 (use-package fsharp-mode
   :ensure t
@@ -99,68 +66,15 @@
 (defun my-fsharp-mode-setup ()
   (define-key fsharp-mode-map (kbd "C-c C-c") #'fsharp-ac/complete-at-point)
   (define-key fsharp-mode-map (kbd "M-p") #'ace-window))
-
 (add-hook 'fsharp-mode-hook #'my-fsharp-mode-setup)
 
 (use-package smex
   :ensure t
   :bind ("M-x" . 'smex))
 
-(defun my-magit-mode-setup ()
-  (define-key magit-mode-map (kbd "M-p") 'ace-window))
-
-(use-package magit
-  :ensure t
-  :bind ("C-c m" . 'magit-status)
-  :config
-  (setq magit-auto-revert-mode nil)
-  (add-hook 'magit-mode-hook 'my-magit-mode-setup)
-  (setq magit-last-seen-setup-instructions "1.4.0"))
-
 (use-package undo-tree
   :ensure t
   :config (global-undo-tree-mode))
-
-
-;;; highlight-focus.el --- highlight the active buffer
-
-(require 'face-remap)
-(defvar highlight-focus:last-buffer nil)
-(defvar highlight-focus:cookie nil)
-(defvar highlight-focus:background "#2e2e25")
-(defvar highlight-focus:app-has-focus t)
-
-(defun highlight-focus:check ()
-  "Check if focus has changed, and if so, update remapping."
-  (let ((current-buffer (and highlight-focus:app-has-focus (current-buffer))))
-	(unless (eq highlight-focus:last-buffer current-buffer)
-	  (when (and highlight-focus:last-buffer highlight-focus:cookie)
-		(with-current-buffer highlight-focus:last-buffer
-		  (face-remap-remove-relative highlight-focus:cookie)))
-	  (setq highlight-focus:last-buffer current-buffer)
-	  (when current-buffer
-		(setq highlight-focus:cookie
-			  (face-remap-add-relative 'default :background highlight-focus:background))))))
-
-(defun highlight-focus:app-focus (state)
-  (setq highlight-focus:app-has-focus state)
-  (highlight-focus:check))
-
-(defadvice other-window (after highlight-focus activate)
-  (highlight-focus:check))
-(defadvice select-window (after highlight-focus activate)
-  (highlight-focus:check))
-(defadvice select-frame (after highlight-focus activate)
-  (highlight-focus:check))
-(add-hook 'window-configuration-change-hook 'highlight-focus:check)
-
-(add-hook 'focus-in-hook (lambda () (highlight-focus:app-focus t)))
-(add-hook 'focus-out-hook (lambda () (highlight-focus:app-focus nil)))
-
-(provide 'highlight-focus)
-
-;;; highlight-focus.el ends here
-
 
 (use-package js2-mode
   :mode "\\.js\\'"
@@ -169,12 +83,6 @@
 (use-package json-mode
   :mode ("\\.json\\'" "\\.unoproj'")
   :ensure t)
-
-;(load-file "~/.emacs.d/material-colors.el")
-
-(use-package undo-tree
-  :config
-  (global-undo-tree-mode))
 
 (defun eshell-with-prefix-arg ()
   (interactive)
@@ -186,14 +94,11 @@
 
 (add-hook 'dired-mode-hook #'dired-config)
 
-(use-package graphviz-dot-mode
-  :ensure t)
 (use-package nodejs-repl
   :ensure t)
 
 (use-package company-lsp
   :ensure t)
-(require 'company-lsp)
 (push 'company-lsp company-backends)
 
 (use-package rust-mode
@@ -205,50 +110,15 @@
   :init
   (setq rust-format-on-save t))
 
-(use-package lsp-mode)
+(use-package lsp-mode
+  :ensure t)
 
-;;(use-package lsp-ui
-;;  :ensure t)
-
-(add-to-list 'load-path "~/lsp-rust/")
-(load "lsp-rust")
-
-
-
-(defun rust-mode-setup ()
-  (lsp-rust-enable))
-
-(add-hook 'rust-mode-hook #'rust-mode-setup)
-
-
-;:config
-;(setq racer-cmd "/Users/Hassel/.cargo/bin/racer")
-;(setq racer-rust-src-path "/Users/Hassel/rust-workspace/rust/src/"))
-
-;(use-package racer-mode
-; :hook rust-mode)
-
-(use-package flycheck-mode
-  :hook (js2-mode typescript-mode))
-
-(use-package eldoc-mode
-  :hook (racer-mode typescript-mode))
-
-(use-package company
-  :ensure t
-  :hook (rust-mode racer-mode typescript-mode))
-
-(defun setup-company-mode ()
-  (setq company-tooltip-align-annotations t)
-  (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort))
-
-(add-hook 'company-mode-hook #'setup-company-mode)
+(use-package eglot
+  :ensure t)
 
 (use-package yaml-mode
   :mode ("\\.yml\\'" "\\.yaml\\'")
   :ensure t)
-
 
 (use-package omnisharp
   :ensure t
@@ -268,10 +138,6 @@
 (use-package csharp-mode
   :ensure t
   :mode ("\\.uno\\'" "\\.cs'"))
-
-(eval-after-load
-  'company
-  '(add-to-list 'company-backends #'company-omnisharp))
 
 (defun my-csharp-mode-setup ()
   (dotnet-mode)
@@ -293,8 +159,11 @@
 
 (add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
 
-(use-package haxe-mode
-  :ensure t)
+(use-package helm
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+  (global-set-key (kbd "C-c y") 'helm-show-kill-ring))
 
 (use-package helm-git-grep
   :bind ("C-c j" . helm-git-grep))
@@ -305,19 +174,9 @@
 
 (use-package rg
   :ensure t)
-
-(defun display-buffer-same-window-override (buffer alist)
-    (window--display-buffer buffer (selected-window) 'reuse alist))
-
-(defun compile-goto-error-same-window ()
-  (interactive)
-  (let ((display-buffer-overriding-action '(display-buffer-same-window-override)))
-    (compile-goto-error)))
-
 (defun my-rg-mode-setup ()
   (define-key rg-mode-map (kbd "M-p") 'ace-window)
   (define-key rg-mode-map (kbd "<C-return>") 'compile-goto-error-same-window))
-
 (add-hook 'rg-mode-hook #'my-rg-mode-setup)
 
 (defun setup-tide-mode (mode-map)
@@ -339,52 +198,39 @@
   (define-key mode-map (kbd "C-c r") 'tide-rename-symbol)
   (define-key mode-map (kbd "C-c i") 'helm-imenu))
 
+(use-package tide
+  :ensure t)
+
 (use-package typescript-mode
   :ensure t
   :mode ("\\.ts\\'" "\\.tsx\\'"))
+(add-hook 'typescript-mode-hook (lambda () (setup-tide-mode typescript-mode-map)))
 
 (use-package web-mode
   :ensure t
   :mode "\\.tsx\\'" "\\.cshtml\\'")
-
-(add-hook 'typescript-mode-hook (lambda () (setup-tide-mode typescript-mode-map)))
 (add-hook 'web-mode-hook
           (lambda ()
             (when (string-equal "tsx" (file-name-extension buffer-file-name))
               (setup-tide-mode web-mode-map))))
 
 (use-package restclient
+  :mode ("\\.http\\'")
   :ensure t)
-
-(add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
-
-
-
-(add-to-list 'company-backends 'company-restclient)
 
 (use-package counsel
   :ensure t)
-
 (global-set-key (kbd "C-M-s") 'counsel-rg)
 (global-set-key (kbd "C-M-f") 'rg-project)
-
-(use-package tide
-  :ensure t)
 
 
 (defun copy-from-osx ()
   (shell-command-to-string "pbpaste"))
-
 (defun paste-to-osx (text &optional push)
   (let ((process-connection-type nil))
 	(let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
 	  (process-send-string proc text)
 	  (process-send-eof proc))))
-
-(use-package rainbow-mode
-  :ensure t)
-
-
 (when (string= system-type "darwin")
   (load-file "~/.emacs.d/reveal-in-finder.el")
   (setq mac-option-modifier nil
@@ -393,23 +239,21 @@
   (setq interprogram-cut-function 'paste-to-osx)
   (setq interprogram-paste-function 'copy-from-osx))
 
+(use-package rainbow-mode
+  :ensure t)
+
 (use-package helm-projectile
+  :ensure t
+  :config
+  (projectile-global-mode)
+  (global-set-key (kbd "C-c t") 'helm-projectile-find-file))(use-package helm-projectile
   :ensure t
   :config
   (projectile-global-mode)
   (global-set-key (kbd "C-c t") 'helm-projectile-find-file))
 
-(require 'tramp)
 
-(use-package helm
-  :ensure t
-  :config
-  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
-  (global-set-key (kbd "C-c y") 'helm-show-kill-ring))
-
- ; helm-projectile to find file in projects
-
-
+;;Change window sizes
 (global-set-key (kbd "S-C-<left>") 'shrink-winndow-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
@@ -422,17 +266,6 @@
   (global-set-key (kbd "C-S-n") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-x r t") 'mc/edit-lines))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(lsp-face-highlight-textual ((t (:background "blue"))))
- '(whitespace-indentation ((t (:foreground "gray29" :weight bold))))
- '(whitespace-tab ((t (:foreground "gray31" :weight bold))))
- '(whitespace-trailing ((t (:foreground "red1" :inverse-video t)))))
-
-
 (defun elisp-mode-setup ()
   (message "initializing elisp")
   (company-mode)
@@ -440,28 +273,20 @@
   (define-key emacs-lisp-mode-map (kbd "C-c C-b") 'edebug-x-modify-breakpoint-wrapper)
   (define-key emacs-lisp-mode-map (kbd "C-c C-l") 'edebug-x-show-breakpoints)
   (define-key emacs-lisp-mode-map (kbd "C-c C-r") 'xref-find-references))
-
-
 (add-hook 'emacs-lisp-mode-hook #'elisp-mode-setup)
-
-(defun edebug-x-breakpoint-list-mode-setup ()
-  (message "initializing breakpoint list mode")
-  (define-key edebug-x-breakpoint-list-mode-map (kbd "RET") 'edebug-x-visit-breakpoint))
-
-(add-hook 'edebug-x-breakpoint-list-mode-hook #'edebug-x-breakpoint-list-mode-setup)
 
 (use-package editorconfig
   :ensure t
   :config
   (editorconfig-mode 1))
 
+;;Delete region when typing
+(delete-selection-mode 1)
 
 ;; Auto refresh buffers
 (global-auto-revert-mode 1)
-
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
-
 (setq shift-select-mode nil)
 
 ;; UTF-8 please
@@ -472,7 +297,6 @@
 (prefer-coding-system 'utf-8) ; with sugar on top
 
 (setq fill-column 80)
-
 (setq gc-cons-threshold 20000000)
 
 ;; org-mode: Don't ruin S-arrow to switch windows please (use M-+ and M-- instead to toggle)
@@ -485,17 +309,16 @@
   (define-key org-mode-map (kbd "M-p") 'ace-window)
   (define-key org-mode-map (kbd "C-j") 'avy-goto-word-1)
   (define-key org-mode-map (kbd "C-c C-v") 'hydra-org-mode/body))
-
 (add-hook 'org-mode-hook #'my-org-mode-setup)
 
+(setq org-directory "~/personal")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(define-key global-map (kbd "C-c c") 'org-capture)
+
+
 (use-package ace-window
-  :ensure t)
-
-(global-set-key (kbd "M-p") 'ace-window)
-
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
+  :ensure t
+  :config (global-set-key (kbd "M-p") 'ace-window))
 
 ;; Offer to create parent directories if they do not exist
 ;; http://iqbalansari.github.io/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
@@ -504,30 +327,14 @@
 	(when (and (not (file-exists-p parent-directory))
 			   (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
 	  (make-directory parent-directory t))))
-
 (add-to-list 'find-file-not-found-functions 'my-create-non-existent-directory)
-
-(setq org-directory "~/Jottacloud/org")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(define-key global-map (kbd "C-c c") 'org-capture)
-
 
 (use-package avy-zap
   :ensure t)
-
 (define-key global-map (kbd "C-j") 'avy-goto-word-1)
-
-(use-package dart-mode
-  :ensure t
-  :mode "\\.dart\\'")
-
-;(add-hook 'c++-mode-hook 'irony-mode)
-;(add-hook 'c-mode-hook 'irony-mode)
-
 
 (defun eshell-setup ()
   (define-key eshell-mode-map (kbd "M-p") 'ace-window))
-
 (add-hook 'eshell-mode-hook 'eshell-setup)
 
 (use-package cargo
@@ -537,22 +344,21 @@
   (define-key rust-mode-map (kbd "M-<f5>") #'cargo-process-test)
   (define-key rust-mode-map (kbd "S-<f5>") #'cargo-process-run)
   (define-key cargo-process-mode-map (kbd "M-p") #'ace-window))
-
 (add-hook 'rust-mode-hook #'setup-cargo-rust-mode)
-
 (add-hook 'cargo-process-mode-hook 'setup-cargo-rust-mode)
 
+(use-package markdown-mode
+  :ensure t
+  :mode ("\\.md\\'"))
+(defun my-markdown-mode-setup ()
+  (define-key markdown-mode-map (kbd "M-p") 'ace-window))
+(add-hook 'markdown-mode-hook #'my-markdown-mode-setup)
 
-;(use-package glsl-mode
-;  :mode ("\\.fs\\'" "\\.vs\\'")
-;  :ensure t)
-
-(use-package reveal-in-osx-finder
-  :ensure t)
-
-(require 's)
-(require 'dash)
 (use-package ht
+  :ensure t)
+(use-package s
+  :ensure t)
+(use-package dash
   :ensure t)
 
 (use-package wgrep
@@ -564,16 +370,6 @@
 (setq auto-save-file-name-transforms
 	  `((".*" ,temporary-file-directory t)))
 
-(use-package markdown-mode
-  :ensure t
-  :mode ("\\.md\\'"))
-
-(defun my-markdown-mode-setup ()
-  (define-key markdown-mode-map (kbd "M-p") 'ace-window))
-
-(add-hook 'markdown-mode-hook #'my-markdown-mode-setup)
-
-;(load-file "./fira-code.el")
 
 ; Just for now to make it bigger on my large screen
 (when (> (x-display-pixel-width) 3000)
@@ -581,23 +377,16 @@
 (when (equal (x-display-pixel-width) 2560)
   (set-face-attribute 'default nil :height 140))
 
-
 (when (and (string= system-type "darwin") (> (x-display-pixel-width) 3000))
   (set-face-attribute 'default nil :height 150))
 
+
 (use-package expand-region
   :ensure t
-  :config (global-set-key (if (string= system-type "darwin")
-							  (kbd "C-@")
-							(kbd "C-'")) 'er/expand-region))
-
-(use-package ts-comint
-  :ensure t)
-
-;(shell-command "npm install -g tsun")
-
-(use-package nodejs-repl
-  :ensure t)
+  :config (global-set-key
+	   (if (string= system-type "darwin")
+	       (kbd "C-@")
+	     (kbd "C-'")) 'er/expand-region))
 
 (defun locate-current-file-in-explorer ()
   (interactive)
@@ -613,11 +402,8 @@
 	(shell-command (concat "start explorer /e,\"" (replace-regexp-in-string "/" "\\\\" (eshell/pwd)) "\"")))
    ;; Use default-directory as last resource
    (t
-	(shell-command (concat "start explorer /e,\"" (replace-regexp-in-string "/" "\\\\" default-directory) "\"")))))
+    (shell-command (concat "start explorer /e,\"" (replace-regexp-in-string "/" "\\\\" default-directory) "\"")))))
 
-
-;;Delete region when typing
-(delete-selection-mode 1)
 
 (use-package neotree
   :ensure t)
@@ -644,7 +430,6 @@ _g_: Revert buffer       _r_: reload solution  _o_: Agenda    _f_: Make frame
 _l_: Whitespace cleanup  _s_: start server                  _d_: Delete frame
 _c_: Compile
 _e_: Error list
-
 "
   ("g" revert-buffer)
   ("l" whitespace-cleanup)
@@ -659,38 +444,17 @@ _e_: Error list
 
 (global-set-key (kbd "M-C-g") 'hydra-global/body)
 
+
 (use-package yasnippet
-  :ensure t)
+  :ensure t
+  :config
+  (yas-global-mode 1)
+  (global-set-key (kbd "C-c <tab>") 'yas-insert-snippet))
 
 (use-package yasnippet-snippets
   :ensure t)
-
-(add-to-list 'yasnippet-snippets-dir "~/.emacs.d/snippets/")
-(yas-global-mode 1)
-
-(global-set-key (kbd "C-c <tab>") 'yas-insert-snippet)
-
-(defun my-compilation-mode-setup ()
-  (define-key compilation-mode-map (kbd "M-p") #'ace-window))
-
-(add-hook 'compilation-mode-hook #'my-compilation-mode-setup)
 
 (use-package calfw
   :ensure t)
 (use-package calfw-org
   :ensure t)
-(use-package org-gcal
-  :ensure t)
-
-
-
-;;(load-file "./slack-integration.el")
-
-
-(defun comment-or-uncomment-region-if-region ()
-  (interactive)
-  (if mark-active
-	  (comment-or-uncomment-region (region-beginning) (region-end))
-	(insert-char ?/)))
-
-(global-set-key (kbd "/") 'comment-or-uncomment-region-if-region)

7983d76dd50b3f4c68d94f5eed0516c5714c4d4e
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2018-08-12 14:01:54 +0200

    show line numbers

--- custom-vars.el
+++ custom-vars.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(display-line-numbers t)
  '(package-selected-packages
    (quote
     (calfw-org calfw yasnippet-snippets cargo avy-zap ace-window editorconfig tide multiple-cursors helm-projectile rainbow-mode counsel helm web-mode typescript-mode rg swiper omnisharp yaml-mode restclient eglot company-lsp nodejs-repl json-mode js2-mode undo-tree smex fsharp-mode company magit use-package))))

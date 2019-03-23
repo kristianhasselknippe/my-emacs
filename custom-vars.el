c6fcbabd76f2199e0e6038dd78468694dc4d8f26
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-03-23 14:36:46 +0100

    small changes

--- custom-vars.el
+++ custom-vars.el
@@ -57,6 +57,9 @@
  '(lsp-ui-sideline-show-code-actions nil)
  '(nxml-child-indent 4)
  '(omnisharp-server-executable-path nil)
+ '(org-agenda-files
+   (quote
+	("~/org/timesheet.org" "~/org/nortero/projects/SNVSpektrum/venyouscanner.org")))
  '(org-clock-persist (quote clock))
  '(package-selected-packages
    (quote
@@ -75,4 +78,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(web-mode-comment-face ((t (:foreground "light pink"))))
  '(whitespace-big-indent ((t nil))))
